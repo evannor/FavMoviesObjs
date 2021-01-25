8306c9bcc9e6adb106e3c6b98e359d2102bf6baa
evannor <erica.vannorman@gmail.com>
2021-01-25 14:26:29 -0800

    Add dyanmic newMovie object

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,11 +1,34 @@
-// object literal notation
-const person = {
-  name: "Emily",
-  age: 30,
-  hobbies: ["RPGs", "Crochet"],
-  greet: () => {
-    alert("Howdy!");
-  },
-};
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
 
-person.greet();
+const movies = [];
+
+function addMovieHandler() {
+  // gather all user input and add that to the movie(create a new movie object)
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
+
+  // validation
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
+    return;
+  }
+
+  const newMovie = {
+    info: {
+      // if key name and value are the same (value is a dynamic variable), simply write that value once
+      // JS will understand and break it into  title: title,
+      title,
+      [extraName]: extraValue,
+    },
+    id: Math.random(),
+  };
+  movies.push(newMovie);
+  console.log(newMovie);
+}
+
+addMovieBtn.addEventListener("click", addMovieHandler);
