5804c09882ad255d3a2cbac4911495b04083511b
evannor <erica.vannorman@gmail.com>
2021-01-25 14:58:43 -0800

    Output dynamic properties

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -18,7 +18,14 @@ function renderMovies() {
 
   movies.forEach((movie) => {
     const movieEl = document.createElement("li");
-    movieEl.textContent = movie.info.title;
+    let text = movie.info.title + " - ";
+    // go through all key-value pairs in an object with for-in loop
+    for (const key in movie.info) {
+      if (key !== "title") {
+        text += `${key}: ${movie.info[key]}`;
+      }
+    }
+    movieEl.textContent = text;
     movieList.append(movieEl);
   });
 }
